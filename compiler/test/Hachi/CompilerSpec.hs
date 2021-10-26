f00181234548c214fd7ec4b58beea3a141a4f035
Michael B. Gale <michael.gale@tweag.io>
2021-10-26 12:09:45 +0100

    Add test framework

---
+++ compiler/test/Hachi/CompilerSpec.hs
@@ -0,0 +1,56 @@
+
+module Hachi.CompilerSpec where
+
+-------------------------------------------------------------------------------
+
+import Control.Monad
+
+import qualified Data.ByteString.Lazy as LBS
+
+import System.Directory
+import System.FilePath
+import System.Process.Typed
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Hachi.Compiler
+import Hachi.Compiler.Config
+
+-------------------------------------------------------------------------------
+
+listDirectories :: FilePath -> IO [FilePath]
+listDirectories root =
+    listDirectory root >>=
+    filterM (doesDirectoryExist . (root </>))
+
+test_samples :: IO [TestTree]
+test_samples = do
+    let root = "./test-data/untyped"
+    let cfg fp = MkConfig{
+            cfgInput = fp,
+            cfgOutput = Nothing,
+            cfgDeserialise = False,
+            cfgTyped = False,
+            cfgTrace = False
+        }
+    ds <- listDirectories root
+    forM ds $ \dir ->
+        pure $ testCase dir $ do
+            -- compile the code
+            let fp = root </> dir </> dir <.> "plc"
+            compile (cfg fp)
+
+            -- run the resulting program
+            let pcfg = proc (dropExtension fp) []
+            (ec, stdout, stderr) <- readProcess pcfg
+
+            -- read the expected output
+            exStdout <- LBS.readFile $ replaceExtension fp "out"
+
+            -- check that the output we got from the program matches the
+            -- expected output
+            assertEqual "Expected output does not match actual output"
+                exStdout stdout
+
+-------------------------------------------------------------------------------
