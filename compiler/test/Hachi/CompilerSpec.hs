857e1c73e51af44a50419adb34120c0d5a07c42d
Michael B. Gale <michael.gale@tweag.io>
2021-12-02 11:35:05 +0000

    Move more test code into `TestCommon`

--- compiler/test/Hachi/CompilerSpec.hs
+++ compiler/test/Hachi/CompilerSpec.hs
@@ -1,17 +1,12 @@
 
-module Hachi.CompilerSpec where
+module Hachi.CompilerSpec (
+    test_untyped_samples,
+    test_typed_samples
+) where
 
 -------------------------------------------------------------------------------
 
-import Control.Monad
-
-import Data.List (sort)
-
-import System.Directory
-import System.FilePath
-
 import Test.Tasty
-import Test.Tasty.HUnit
 
 import Hachi.Compiler
 import Hachi.Compiler.Config
@@ -19,34 +14,17 @@ import Hachi.TestCommon
 
 -------------------------------------------------------------------------------
 
-listDirectories :: FilePath -> IO [FilePath]
-listDirectories root =
-    listDirectory root >>=
-    filterM (doesDirectoryExist . (root </>))
-
--- | `runDirectoryTests` @mkConfig root@ loads programs from all directories in
--- the path given by @root@ using the configuration produced by applying
--- @mkConfig@ to a given source file's path.
-runDirectoryTests :: (FilePath -> Config) -> FilePath -> IO [TestTree]
-runDirectoryTests mkCfg root = do
-    ds <- listDirectories root
-
-    forM (sort ds) $ \dir ->
-        pure $ testCase dir $ do
-            -- compile the code
-            let fp = root </> dir </> dir <.> "plc"
-            compile (mkCfg fp)
-
-            -- run the resulting program
-            runAndVerify fp
-
 test_untyped_samples :: IO [TestTree]
-test_untyped_samples = runDirectoryTests mkDefaultConfig "./test-data/untyped"
+test_untyped_samples =
+    runDirectoryTests "./test-data/untyped" $ \fp ->
+    compile (mkDefaultConfig fp) >> runAndVerify fp
 
 mkTypedConfig :: FilePath -> Config
 mkTypedConfig fp = (mkDefaultConfig fp){ cfgTyped = True }
 
 test_typed_samples :: IO [TestTree]
-test_typed_samples = runDirectoryTests mkTypedConfig "./test-data/typed"
+test_typed_samples =
+    runDirectoryTests "./test-data/typed" $ \fp ->
+    compile (mkTypedConfig fp) >> runAndVerify fp
 
 -------------------------------------------------------------------------------
